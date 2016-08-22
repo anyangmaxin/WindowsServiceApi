b0377cd6f3bcc25345fa436c74b5006507afc87e
马鑫 <anyangmaxin@163.com>
2016-08-22 14:56:53 +0800

    移除已经绑定的域名

--- Microsoft.Web.Administrator/Program.cs
+++ Microsoft.Web.Administrator/Program.cs
@@ -12,25 +12,32 @@ namespace Microsoft.Web.Administrator
     {
         static void Main(string[] args)
         {
-            StringBuilder stringBuilder=new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             ServerManager serverManager = new ServerManager();
+            //所有iis站点
             SiteCollection siteCollection = serverManager.Sites;
+            Binding removeBinding=null;
             foreach (Site site in siteCollection)
             {
                 //retrieve the State of the Site
-                stringBuilder.AppendFormat(" site.Name:{0},site.State:{1},site.Id:{2}\r\n", site.Name,site.State,site.Id);
-              
+                stringBuilder.AppendFormat(" site.Name:{0},site.State:{1},site.Id:{2}\r\n", site.Name, site.State, site.Id);
+
                 //Get the Binding objects for this Site
                 BindingCollection bindingCollection = site.Bindings;
                 foreach (Binding binding in bindingCollection)
                 {
                     //put code here to work with each Binding
-                    stringBuilder.AppendFormat("\t binding.Host:{0} \r\n",binding.Host);
+                    stringBuilder.AppendFormat("\t binding.Host:{0} \r\n", binding.Host);
                     stringBuilder.AppendFormat("\t binding.Protocol:{0} \r\n", binding.Protocol);
                     stringBuilder.AppendFormat("\t binding.CertificateHash:{0} \r\n", binding.CertificateHash);
-                    stringBuilder.AppendFormat("\t binding.BindingInformation:{0} \r\n",  binding.BindingInformation);
+                    stringBuilder.AppendFormat("\t binding.BindingInformation:{0} \r\n", binding.BindingInformation);
                     stringBuilder.AppendFormat("\t binding.IsIPPortHostBinding:{0} \r\n", binding.IsIPPortHostBinding);
                     stringBuilder.AppendFormat("\t ------------------------------------------- \r\n");
+                    if (binding.Host == "www.baidu.com")
+                    {
+                        removeBinding = binding;
+                   
+                    }
                 }
 
                 //Get the list of all Applications for this Site
@@ -38,7 +45,7 @@ namespace Microsoft.Web.Administrator
                 foreach (Application application in applicationCollection)
                 {
                     //put code here to work with each Application
-                   // stringBuilder.AppendFormat("\t\t application.ApplicationPoolName：{0},application.Schema:{1}\r\n", application.ApplicationPoolName, application.Schema);
+                    // stringBuilder.AppendFormat("\t\t application.ApplicationPoolName：{0},application.Schema:{1}\r\n", application.ApplicationPoolName, application.Schema);
                 }
 
                 ConfigurationAttributeCollection attributeCollection = site.Attributes;
@@ -47,19 +54,26 @@ namespace Microsoft.Web.Administrator
                     //put code here to work with each ConfigurationAttribute
                 }
 
+                if (removeBinding != null)
+                {
+                    site.Bindings.Remove(removeBinding);
+                }
+          
+
             }
 
             Console.WriteLine(stringBuilder.ToString());
-            Console.WriteLine("-----------------");
-            string read=Console.ReadLine();
+            Console.WriteLine("输入需要绑定域名的站点索引,从1开始");
+            string read = Console.ReadLine();
             int id = Convert.ToInt32(read);
-            var a=siteCollection.SingleOrDefault(m => m.Id == id);
-           // a.Stop();
+            var a = siteCollection.SingleOrDefault(m => m.Id == id);
+            // a.Stop();
 
-           //绑定域名
+            //绑定域名
             a.Bindings.Add("*:80:test.baidu.com", "http");
             //*:80:admin.localtgjs.com
             serverManager.CommitChanges();
+
             Console.ReadKey();
 
 
